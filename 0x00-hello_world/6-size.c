1af55b667fb9e08e391d2ebac5b5d68b534262eb
fk2019 <fkmuiruri8@gmail.com>
2022-09-09 12:52:13 -0700

    Add package

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of a int: %lu%s\n", sizeof(i),"byte(s)");
 	printf("Size of a long int: %lu%s\n", sizeof(li),"byte(s)");
 	printf("Size of a long long int: %lu%s\n", sizeof(lli),"byte(s)");
-	printf("Size of a float: %lu\%sn", sizeof(f),"byte(s)");
+	printf("Size of a float: %lu%s\n", sizeof(f),"byte(s)");
 	return (0);
 }
