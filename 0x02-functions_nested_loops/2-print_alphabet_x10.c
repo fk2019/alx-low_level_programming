60b66ea8f3f1ce9936b4b6aec9501a53d17754a0
fk2019 <fkmuiruri8@gmail.com>
2022-09-14 09:22:36 -0700

    Add function description

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,9 @@
 /**
 * 2-print_alphabet_x10 - prints 10 times the alphabet
 *
-* Description: func prints the alphabet 10 times
+* Description: print_alphabet_x10 function prints the alphabet 10 times. It
+* uses two loops with the inside loop printing the alphabet and main loop
+* printing output 10 times.
 * Return: void
 */
 void print_alphabet_x10(void)
