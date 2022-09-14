5c82983c1dc9d106356ffdd59bd55ac54a322734
fk2019 <fkmuiruri8@gmail.com>
2022-09-14 11:23:15 -0700

    Add abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - computes absolute values
+* @i: number parameter to be checked
+*
+* Return: absolute number
+*/
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else if (n == 0)
+		return (0);
+	return (-n);
+}
