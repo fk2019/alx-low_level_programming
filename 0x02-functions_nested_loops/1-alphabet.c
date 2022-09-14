3b99ec13b8fd873401af4e909488afec863f3068
fk2019 <fkmuiruri8@gmail.com>
2022-09-14 08:31:01 -0700

    Add alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - prints all alphabet
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
