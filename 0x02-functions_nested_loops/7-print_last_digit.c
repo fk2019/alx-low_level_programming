fc93957095e5d840da4738270cddabb81e49b34e
fk2019 <fkmuiruri8@gmail.com>
2022-09-14 11:44:01 -0700

    Add print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_last_digit - computes absolute values
+* @n: number parameter to be checked
+*
+* Return: last digit
+*/
+int print_last_digit(int n)
+{
+	int d = n % 10;
+
+	if (d < 0)
+		d *= -1;
+
+	_putchar(d + '0');
+	return (d);
+}
