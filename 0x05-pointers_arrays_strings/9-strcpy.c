3e6e970923a6e1a5fb81a3394ecb7b6bcb42b6fe
fk2019 <fkmuiruri8@gmail.com>
2022-09-22 12:30:13 -0700

    Add strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _strcpy - copy string
+* @dest: first operand
+* @src: second operand
+*
+* Return: pointer to dest
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+
+	for (len = 0; *(dest + len) != *(src + len); len++)
+		*(dest + len) = *(src + len);
+
+	*(dest + len) = '\0';
+	return (dest);
+}
