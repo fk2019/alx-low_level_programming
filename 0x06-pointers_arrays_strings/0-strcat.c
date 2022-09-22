86d829d23c3b6ed061d5f8493a68fd5e8b6ee9c6
fk2019 <fkmuiruri8@gmail.com>
2022-09-22 14:34:28 -0700

    Edit loop

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,25 +9,11 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int destl = 0;
-	int srcl = 0;
-	int i, j = 0;
+	while (*(++dest))
+	{}
 
-	while (*dest != 0)
-	{
-		dest++;
-		destl++;
-	}
-	while (*src != 0)
-	{
-		src++;
-		srcl++;
-	}
-	for (i = destl; src[j] != '\0'; i++)
-	{
-		dest[i] = src[j];
-		j++;
-	}
-	src[i] = '\0';
+	while ((*(dest++) = *(src++)))
+	{}
+
 	return (dest);
 }
