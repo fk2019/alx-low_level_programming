edf60c239b0d7ae59ae7b55c2f33778d9940f0da
fk2019 <fkmuiruri8@gmail.com>
2022-09-25 07:29:20 -0700

    Add quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+* main - prints string to standard error
+*
+* Return: 1
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpa, 2015-10-19\n", 59);
+	return (1);
+}
