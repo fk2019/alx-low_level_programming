1923210984a11d7f1852ebdb4feb60b4a6d55cd1
fk2019 <fkmuiruri8@gmail.com>
2022-10-02 05:51:22 -0700

    Add palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+* _strlen_recursion - check length of string
+* *s: string to check
+*
+* Return: length of string
+*/
+int _strlen_recursion(char *s)
+{
+	int n = 1;
+
+	if (*s == '\0')
+		return (0);
+	return (n + _strlen_recursion(s + 1));
+}
+/**
+* comp_recursion - check characters of string
+* @s: string
+* @start: beginning of string
+* @end: from end of string
+*
+* Return: 1 if palindrome else 0
+*/
+int comp_recursion(char *s, int start, int end)
+{
+	if (*(s + start) == *(s + end))
+	{
+		if (start == end || start == end + 1)
+			return (1);
+		return (comp_recursion(s, start + 1, end - 1));
+	}
+	return (0);
+}
+/**
+* is_palindrome - checks if string is palindrome
+* @s: string to check
+*
+* Return: 1 if palindrome else 0
+*/
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comp_recursion(s, 0, _strlen_recursion(s) - 1));
+}
