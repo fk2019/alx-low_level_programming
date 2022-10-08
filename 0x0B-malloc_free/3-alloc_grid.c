a72a37d47c8292e1a05a79a3ccbc0c6251d2f5b9
fk2019 <fkmuiruri8@gmail.com>
2022-10-08 06:55:58 -0700

    Add grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - return pointer to 2 dimesional array of ints
+ * @width: width of array
+ * @height: height of array
+ *
+ * Return: pointer to array, NULL on failure, NULL if widht/height = 0
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = malloc(sizeof(int) * height);
+	if (grid == NULL)
+	{
+		free(grid);
+		return (NULL);
+	}
+	i = 0;
+
+	while (i < height)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+		i++;
+	}
+	return (grid);
+}
