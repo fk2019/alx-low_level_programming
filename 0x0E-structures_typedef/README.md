c1e75041fb521a97a78c9fc2bbaaf04aa3f87582
fk2019 <fkmuiruri8@gmail.com>
2022-10-13 04:32:22 -0700

    Add dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+# 0x0E-structures_typedef - C structures and typedef
+
+## Tasks
+
+## My thoughts
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - defines dog structure
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*_DOG_H_ */
