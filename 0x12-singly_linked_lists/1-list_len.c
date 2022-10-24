6cab931f4118cfdc0b1473f9c000ff88933bd5de
fk2019 <fkmuiruri8@gmail.com>
2022-10-24 09:59:52 -0700

    Add listlen

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in list_t
+ * @h: list_t
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+	while (h != NULL)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
