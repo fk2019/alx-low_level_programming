c68bf49de06e252f6af43bfe81f2a51ad4dc9b08
fk2019 <fkmuiruri8@gmail.com>
2022-10-27 10:37:46 -0700

    Add poplist

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * po_listint - deletes head node of listint_t
+ * @head: head
+ *
+ * Return: head node's data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int n;
+
+	current = *head;
+	*head = (*head)->next;
+	n = current->n;
+	free(current);
+	return (n);
+}
