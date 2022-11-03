c03dff25d0f83ac84eba2d4f96bc41ff062115e8
fk2019 <fkmuiruri8@gmail.com>
2022-11-03 02:45:37 -0700

    Edit func

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	ssize_t l;
 	int n, fd;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	if (filename == NULL)
