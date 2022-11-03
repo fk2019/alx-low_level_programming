063fb322c3aa80f54cf44adc2a0d69fd1b1d9d29
fk2019 <fkmuiruri8@gmail.com>
2022-11-03 02:41:53 -0700

    Edit func

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,18 +18,19 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (-1);
-	p = text_content;
-	n = 0;
-	while (*p)
+	if (text_content)
 	{
-		n++;
-		p++;
+		p = text_content;
+		n = 0;
+		while (*p)
+		{
+			n++;
+			p++;
+		}
+		l = write(fd, text_content, n);
+		if (l == -1)
+			return (-1);
 	}
-	l = write(fd, text_content, n);
-	if (l == -1)
-		return (-1);
 	close(fd);
 	return (1);
 }
