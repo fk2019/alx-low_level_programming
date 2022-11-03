1d50b2918a0ef201ebaaad6a799b3aef8d55f024
fk2019 <fkmuiruri8@gmail.com>
2022-11-03 02:39:06 -0700

    Edit func

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,6 +18,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (filename == NULL)
 		return (-1);
+	if (text_content == NULL)
+		return (-1);
 	p = text_content;
 	n = 0;
 	while (*p)
