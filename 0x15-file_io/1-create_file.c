b29e9f788eeec477b74f9e8fc381b35cee62f474
fk2019 <fkmuiruri8@gmail.com>
2022-11-03 02:25:56 -0700

    Add createfile

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: filename
+ * @text_content: text content
+ *
+ * Return: 1 or -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	char *p;
+	ssize_t l;
+	int n, fd;
+
+	fd = open(filename, O_CREAT | O_WRONLY, 600);
+	if (fd == -1)
+		return (-1);
+	if (filename == NULL)
+		return (-1);
+	p = text_content;
+	n = 0;
+	while (*p)
+	{
+		n++;
+		p++;
+	}
+	l = write(fd, text_content, n);
+	if (l == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
