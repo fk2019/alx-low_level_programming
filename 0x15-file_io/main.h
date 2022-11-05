65ef50ca577c336b2dcc5598382f3fbc2842064f
fk2019 <fkmuiruri8@gmail.com>
2022-11-05 12:10:08 -0700

    Add main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,8 +6,14 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <elf.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+typedef struct ELF64{
+	uint8_t e_ident[16];
+	uint16_t e_type;
+	uint64_t e_entry;
+} ELF64Hdr;
 #endif /* _MAIN_H_ */
