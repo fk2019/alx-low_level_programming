9e3d736f342a739ca23be628d5e33f93f2150bb9
fk2019 <fkmuiruri8@gmail.com>
2023-01-15 11:45:29 -0800

    Add 3

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -33,4 +33,8 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
+hash_node_t *set_pair(const char *key, const char *value);
+int set_pair_only(hash_table_t *ht, const char *key,
+		  const char *value, unsigned long int index);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 #endif /*_HASH_TABLE_H_*/
